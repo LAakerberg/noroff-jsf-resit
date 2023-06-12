be64e91b30b2740c6ed9307dd40f99968c00d26e
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-13 01:18:56 +0200

    Message status if success or error.

---
+++ src/components/Pokemon/Message/index.jsx
@@ -0,0 +1,22 @@
+import { Spinner } from '../Spinner';
+
+/**
+ * Component for displaying a message with an optional spinner.
+ *
+ * @param {Object} props - The component props.
+ * @param {string} props.type - The type of the message (e.g., 'success', 'error').
+ * @param {string} props.text - The text content of the message.
+ * @returns {JSX.Element} The Message component.
+ */
+export const Message = ({ type, text }) => {
+  return (
+    <div className={`message ${type}`}>
+      <div className="flex-1">
+        <p className="m-2">{text}</p>
+      </div>
+      <div className="flex-initial p-1">
+        {type !== 'error' && type !== 'booked' && <Spinner />}
+      </div>
+    </div>
+  );
+};
