9b84ad3a8df5feb842c90cd7749f3ec0bf98cb95
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-12 17:34:43 +0200

    Added react router + header style config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,39 @@
 /** @type {import('tailwindcss').Config} */
+
+import plugin from 'tailwindcss/plugin';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '400px',
+        // => @media (min-width: 400px) { ... }
+        tablet: '500px',
+        // => @media (min-width: 400px) { ... }
+        sm: '640px',
+        // => @media (min-width: 640px) { ... }
+        md: '768px',
+        // => @media (min-width: 640px) { ... }
+        lg: '1024px',
+        // => @media (min-width: 1024px) { ... }
+        xl: '1280px',
+        // => @media (min-width: 1280px) { ... }
+        '2xl': '1536px',
+        // => @media (min-width: 1536px) { ... }
+        '3xl': '1836px',
+        // => @media (min-width: 1836px) { ... }
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: { fontSize: theme('fontSize.4xl') },
+        h2: { fontSize: theme('fontSize.3xl') },
+        h3: { fontSize: theme('fontSize.2xl') },
+        h4: { fontSize: theme('fontSize.xl') },
+      });
+    }),
+  ],
 };
