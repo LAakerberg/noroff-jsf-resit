b23620d7fa2883cf60298a7a85b86eb25e8a4ee7
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-13 01:17:15 +0200

    spinner to use when loading from API

---
+++ src/components/Pokemon/Spinner.jsx
@@ -0,0 +1,10 @@
+/**
+ * Component for displaying a spinner.
+ *
+ * @example
+ * // Renders the Spinner component, indicating loading or processing.
+ * <Spinner />
+ */
+export function Spinner() {
+  return <div className="spinner pr-2"></div>;
+}
