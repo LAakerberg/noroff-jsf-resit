3f026ad16b1332da750b0b7f85ce3a2318ba087d
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-14 17:06:28 +0200

    Removed H3

--- src/pages/Pokemon/index.jsx
+++ src/pages/Pokemon/index.jsx
@@ -3,7 +3,6 @@ import { FetchPokemon } from '../../components/Pokemon/FetchPokemon';
 export function Pokemon() {
   return (
     <>
-      <h3>Pokemon</h3>
       <div>
         <FetchPokemon />
       </div>
