d8428a83c21bd06dff632809ac4c4bb165fde1a8
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-18 02:14:17 +0200

    CHanged H1 to a bigger size

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,7 +31,7 @@ export default {
   plugins: [
     plugin(function ({ addBase, theme }) {
       addBase({
-        h1: { fontSize: theme('fontSize.4xl') },
+        h1: { fontSize: theme('fontSize.5xl') },
         h2: { fontSize: theme('fontSize.3xl') },
         h3: { fontSize: theme('fontSize.2xl') },
         h4: { fontSize: theme('fontSize.xl') },
