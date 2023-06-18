6c48508baef588b5e8410610f45ed434a916b25c
Linus Aakerberg <44141432+LAakerberg@users.noreply.github.com>
2023-06-18 16:14:25 +0200

    Added font size and removed one link

--- src/layout/Navigation/Nav.jsx
+++ src/layout/Navigation/Nav.jsx
@@ -26,11 +26,6 @@ export function Nav() {
               Home
             </Link>
           </li>
-          {/* <li>
-            <Link to="/pokemon" className="links" onClick={closeMenu}>
-              Pokemon
-            </Link>
-          </li> */}
           <li>
             <Link to="/contact" className="links" onClick={closeMenu}>
               Contact
